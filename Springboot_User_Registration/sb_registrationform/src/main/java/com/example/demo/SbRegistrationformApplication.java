21697fd633ff70606006f4896c9d0768a0031679
aleefarizwana <126440963+aleefarizwana@users.noreply.github.com>
2023-09-09 23:18:54 +0530

    SbRegistrationformApplication.java
    write the code in SbRegistrationformApplication.java file created in 
    /sb_registrationform/src/main/java/com/example/demo/SbRegistrationformApplication.java
    
    this structure can be easily avaliable ready made from spring tool suite 4

---
+++ Springboot_User_Registration/sb_registrationform/src/main/java/com/example/demo/SbRegistrationformApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRegistrationformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRegistrationformApplication.class, args);
+		
+		System.out.println("Application");
+	}
+
+}
